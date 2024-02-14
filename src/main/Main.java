75838f760e6037da413691fc29d15606cb6a7617
Karol Petryniak <karol.petryniak@gmail.com>
2024-02-14 21:41:32 +0100

    changed main

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,12 +6,11 @@ public class Main {
         int[] array = {1, 34, 65, 74, 123, 453, 654, 867, 987, 1024, 2782};
         int target = 654;
 
-        System.out.println("Linear Search: target is at index " + LinearSearch.search(array, target));
-        System.out.println("Binary Search: target is at index " + BinarySearch.search(array, target));
-        System.out.println("Interpolation Search: target is at index " + InterpolationSearch.search(array, target));
-        System.out.println("Jump Search: target is at index " + JumpSearch.search(array, target));
-        System.out.println("Ternary Search: target is at index " + TernarySearch.search(array, target));
-        System.out.println("Exponential Search: target is at index " + ExponentialSearch.search(array, target));
+        //change LinearSearch to any other class you want and see that output is the same
+        int index = LinearSearch.search(array, target);
+
+        System.out.println("Linear Search: target is at index " + index);
+
     }
 
 }
