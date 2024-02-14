d62ca865bc21c2423baa13a09fa41e3f212b518f
Karol Petryniak <karol.petryniak@gmail.com>
2024-02-14 18:43:02 +0100

    added ternary search

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int[] array = {1, 34, 65, 74, 123, 453, 654, 867, 987, 1024, 2782};
         int target = 654;
 
-        int result = jumpSearch(array, target);
+        int result = ternarySearch(array, target);
 
         if (result == -1) System.out.println("Element not found");
         else System.out.println("Element found at index " + result);
@@ -61,6 +61,26 @@ public class Main {
         }
         for (int i = start; i <= end; i++) {
             if (array[i] == target) return i;
+        }
+
+        return -1;
+    }
+
+    static int ternarySearch(int[] array, int target) {
+        int high = array.length;
+        int low = 0;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 3;
+            int mid2 = high - (high - low) / 3;
+
+            if (array[mid1] == target) return mid1;
+            else if (array[mid2] == target) return mid2;
+            if (target < array[mid1]) high = mid1 - 1;
+            else if (target > array[mid2]) low = mid2 + 1;
+            else {
+                low = mid1 + 1;
+                high = mid2 - 1;
+            }
         }
 
         return -1;
